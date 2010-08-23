7579c02b06837bd14822d2bf9e4d4da787b12dae
Dave Giunta <dgiunta@mac.com>
2010-08-23 16:16:53 -0500

    Adding a linear layout in addition to the centered layout.

--- public/js/main.js
+++ public/js/main.js
@@ -9,15 +9,16 @@ $(function() {
                        + '    <li class="time">:lastBuildTime</li>'
                        + '  </ul>'
                        + '</li>';
-
+  
+  setupNav();
   getProjects();
-  setInterval(getProjects, 5000);
+  // setInterval(getProjects, 5000);
 
   function getProjects () {
     passing.empty();
     failing.empty();
     building.empty();
-    loadFrom('10.98.0.222:8080');
+    loadFrom('10.98.0.103:8080');
     loadFrom('10.98.0.85:8080');
   }
 
@@ -41,5 +42,25 @@ $(function() {
         project.attr('lastBuildStatus') === 'Success' ? passing.append(newProject) : failing.append(newProject);
       }
     });
+  }
+  
+  function setupNav () {
+    var navContainer = $('.nav'),
+        navItems = $('li a', navContainer),
+        classnames = navItems.map(function(i, a) { return a.href.replace(/^.*\#/, ''); }).toArray();
+    
+    navItems.click(selectNav);
+    
+    window.location.hash ? 
+      navItems.filter('[href$=' + window.location.hash.replace(/\#/, '') + ']').click() : 
+      navItems.filter('.selected').click();
+    
+    function selectNav (e) {
+      var link = $(e.target);
+      
+      navItems.removeClass('selected');
+      link.addClass('selected');
+      $('body').removeClass(classnames.join(", ")).addClass(link.attr('href').replace(/\#/, ''));
+    }
   }
 });
